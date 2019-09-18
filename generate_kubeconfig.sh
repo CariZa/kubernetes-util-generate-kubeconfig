2fed15f7ab0ff4dfd3983d36bdd2fff055a449ae
Cari Liebenberg <liebenbc@yahoo.com>
2019-09-18 14:23:49 +0000

    Fixed up some of the template refs and generate script

--- generate_kubeconfig.sh
+++ generate_kubeconfig.sh
@@ -6,22 +6,24 @@
 
 . ./envs.sh
 
-cp ./templates/kubeconfig_data_template.yaml ./kubeconfig_data.yaml
-cp ./templates/kubeconfig_files_template.yaml ./kubeconfig_files.yaml
+cp ./templates/kubeconfig_data_template.yaml ./generated/kubeconfig_data.yaml
+cp ./templates/kubeconfig_files_template.yaml ./generated/kubeconfig_files.yaml
 
-sed -i -e "s|#CACRT_DATA|$CACRT_DATA|g" kubeconfig_data.yaml
-sed -i -e "s|#CLIENTCRT_DATA|$CLIENTCRT_DATA|g" kubeconfig_data.yaml
-sed -i -e "s|#CLIENTKEY_DATA|$CLIENTKEY_DATA|g" kubeconfig_data.yaml
-sed -i -e "s|#SERVERADDRESS|$SERVERADDRESS|g" kubeconfig_data.yaml
-sed -i -e "s|#CLUSTERNAME|$CLUSTERNAME|g" kubeconfig_data.yaml
-sed -i -e "s|#USERNAME|$USERNAME|g" kubeconfig_data.yaml
+sed -i -e "s|#CACRT_DATA|$CACRT_DATA|g" ./generated/kubeconfig_data.yaml
+sed -i -e "s|#CLIENTCRT_DATA|$CLIENTCRT_DATA|g" ./generated/kubeconfig_data.yaml
+sed -i -e "s|#CLIENTKEY_DATA|$CLIENTKEY_DATA|g" ./generated/kubeconfig_data.yaml
+sed -i -e "s|#SERVERADDRESS|$SERVERADDRESS|g" ./generated/kubeconfig_data.yaml
+sed -i -e "s|#CLUSTERNAME|$CLUSTERNAME|g" ./generated/kubeconfig_data.yaml
+sed -i -e "s|#USERNAME|$USERNAME|g" ./generated/kubeconfig_data.yaml
+sed -i -e "s|#NAMESPACE|$NAMESPACE|g" ./generated/kubeconfig_data.yaml
+sed -i -e "s|#DEFAULTCONTEXT|$DEFAULTCONTEXT|g" ./generated/kubeconfig_data.yaml
 
-
-sed -i -e "s|#CACRT_FILE|$CACRT_FILE|g" kubeconfig_files.yaml
-sed -i -e "s|#CLIENTCRT_FILE|$CLIENTCRT_FILE|g" kubeconfig_files.yaml
-sed -i -e "s|#CLIENTKEY_FILE|$CLIENTKEY_FILE|g" kubeconfig_files.yaml
-sed -i -e "s|#SERVERADDRESS|$SERVERADDRESS|g" kubeconfig_files.yaml
-sed -i -e "s|#CLUSTERNAME|$CLUSTERNAME|g" kubeconfig_files.yaml
-sed -i -e "s|#USERNAME|$USERNAME|g" kubeconfig_files.yaml
-
+sed -i -e "s|#CACRT_FILE|$CACRT_FILE|g" ./generated/kubeconfig_files.yaml
+sed -i -e "s|#CLIENTCRT_FILE|$CLIENTCRT_FILE|g" ./generated/kubeconfig_files.yaml
+sed -i -e "s|#CLIENTKEY_FILE|$CLIENTKEY_FILE|g" ./generated/kubeconfig_files.yaml
+sed -i -e "s|#SERVERADDRESS|$SERVERADDRESS|g" ./generated/kubeconfig_files.yaml
+sed -i -e "s|#CLUSTERNAME|$CLUSTERNAME|g" ./generated/kubeconfig_files.yaml
+sed -i -e "s|#USERNAME|$USERNAME|g" ./generated/kubeconfig_files.yaml
+sed -i -e "s|#NAMESPACE|$NAMESPACE|g" ./generated/kubeconfig_files.yaml
+sed -i -e "s|#DEFAULTCONTEXT|$DEFAULTCONTEXT|g" ./generated/kubeconfig_files.yaml
 
